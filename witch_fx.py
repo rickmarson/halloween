1f69474481b74d611a9ebd5495d61d93fcf9be6c
rickmarson <marson.riccardo@gmail.com>
2021-10-11 09:21:16 +0100

    - play sound whenever motion is detected

--- witch_fx.py
+++ witch_fx.py
@@ -1,54 +1,48 @@
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
+from pygame import mixer
 import time
 
 # globals
 LED_PIN = 2 #I2C SDA 
-SWITCH_PIN = 15 #UART RX
 MOTION_SENSOR_PIN = 14 # UART TX
 
 def GpioConfig():
     GPIO.setmode(GPIO.BCM)
 
     GPIO.setup(LED_PIN, GPIO.OUT)
-    GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(MOTION_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
  
     GPIO.output(LED_PIN, GPIO.LOW)
 
+
+def MixerConfig():
+    mixer.init()
+    return mixer.Sound('witchlaf2.wav')
+
 # MAIN
 
 GpioConfig()
-
-switchPressed = False
+sound = MixerConfig()
 
 startTime = time.time()
 
 try:
     while True:
-
-        if GPIO.input(MOTION_SENSOR_PIN):
-            pass
-
-        if GPIO.input(SWITCH_PIN):
-            switchPressed = True 
-        else:
-            if (switchPressed):
-                pass
-
-        switchPressed = False
-
-        # monitor the light state continously so that the led state can be changes even if we toggle the light from other system (e.g. Alexa)
+        
         timeNow = time.time()
-        if(timeNow - startTime >= 1.0):
-            ChangeLedState()
-            startTime = time.time()
+        if GPIO.input(MOTION_SENSOR_PIN) and (timeNow - startTime >= 4.0):
+            print('Motion Detected!')
+            sound.play()
+
+            startTime = timeNow
 
 except KeyboardInterrupt:
     pass
 
 
+mixer.quit()
 GPIO.cleanup()
 
 
